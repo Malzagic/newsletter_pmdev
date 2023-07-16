78a84de1c016f3ada3ae22799ecf492903979374
Malzagic <przemek2142@gmail.com>
2023-07-16 21:10:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Newsletter app
+Hello, welcome on my simple newsletter site created with express.js.
---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+    const { firstName, lastName, email } = req.body;
+
+    const apiKey = "704a370820df63874782989225b737a4-us21";
+    const audienceID = "a5dc13aae8";
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                },
+            }
+        ],
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = `https://us21.api.mailchimp.com/3.0/lists/${audienceID}`;
+    const options = {
+        method: "POST",
+        auth: `przemek2142@gmail.com:${apiKey}`,
+    }
+
+    const request = https.request(url, options, (response) => {
+
+        response.on("data", (data) => {
+            const getData = JSON.parse(data)
+
+            if (getData.error_count === 1) res.sendFile(__dirname + "/failure.html");
+            else res.sendFile(__dirname + "/success.html");
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", (req, res) => {
+    res.redirect("/")
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at: ${port} port`);
+});
